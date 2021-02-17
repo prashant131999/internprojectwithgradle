1291ccd648132d3f3510388d85e3614aed5c078b
prashant <prashant.sahrawat@zopsmart.com>
2021-02-18 01:34:35 +0530

    schema changes done | prashant | postgres code updated

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,17 @@
-INSERT INTO customer VAlUES(1,'prashant','meerut',6923923793);
-INSERT INTO cart     VALUES(11,1,21,'sherlock holmes',32323.9);
-INSERT INTO products VALUES(21,'sherlock holmes','book');
+ALTER TABLE customer DROP COLUMN mobile_number;
+INSERT INTO customer VAlUES(1,'prashant','meerut','6923923793');
+INSERT INTO customer VAlUES(2,'sahrawat','delhi','2429482048');
+INSERT INTO customer VAlUES(3,'gupta','bangalore','8204824080');
+INSERT INTO customer VAlUES(4,'agarwal','mumbai','9292942442');
+INSERT INTO products VALUES(21,'sherlock holmes','books',463.54);
+INSERT INTO products VALUES(22,'laptop','electronics',20000.7);
+INSERT INTO products VALUES(23,'tshirt','cloths',15000.3);
+INSERT INTO cart     VALUES(11,1,21,'sherlock holmes',current_timestamp,current_timestamp,463.54);
+INSERT INTO cart     VALUES(12,2,22,'laptop',current_timestamp,current_timestamp,20463.54);
+INSERT INTO cart     VALUES(13,3,23,'tshirt',current_timestamp,current_timestamp,35463.54);
+
+
+SELECT * FROM customer;
+SELECT * FROM products;
+SElECT * FROM cart;
+ALTER TABLE customer ADD mobile_number VARCHAR(15);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,22 +1,31 @@
-CREATE DATABASE eccomerce;
+CREATE DATABASE db;
 
-create table customer(
-customer_id INT primary key,
-name varchar(250),
-address varchar(250),
+CREATE TABLE customer(
+customer_id INT PRIMARY KEY,
+name VARCHAR(250),
+address VARCHAR(250),
 mobile_number INT(12)
 );
 
-create table cart(
-cartid INT primary key,
-user_id INT foreign key REFERENCES(customer_id),
-prod_id  INT  foreign key REFERENCES(prodid),
-products VARCHAR(250),
-totalvalue float(100)
+CREATE TABLE cart(
+cartid INT NOT NULL,
+customer_id INT,
+prodid INT,
+products VARCHAR(250) NOT NULL,
+date_created TIMESTAMP,
+date_modified TIMESTAMP,
+totalvalue FLOAT(50),
+PRIMARY KEY (cartid),
+FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
+FOREIGN KEY (prodid)  REFERENCES products(prodid)
 );
 
-create products(
-prodid INT primary key,
-product_name varchar(250),
-product_description varchar(250)
+CREATE TYPE categories AS ENUM('electronics','books','cloths');
+
+CREATE TABLE products(
+prodid INT NOT NULL,
+product_name VARCHAR(250),
+product_description categories,
+product_cost float(50),
+PRIMARY KEY(prodid)
 );
